e0c85d7749cbd7efa51add6b4b74bf57d4eb66d4
Sina Shabani <sina.sh.k78@gmail.com>
2019-06-14 13:20:52 +0430

    sync

---
+++ src/TSP/api/Views.java
@@ -0,0 +1,11 @@
+package TSP.api;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Views extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
